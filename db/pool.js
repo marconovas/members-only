a922feeeba6ea3572360b0a2dc6bb401bd05d7a8
Marco Novas <marco.nicolas.novas@gmail.com>
2026-04-12 01:02:39 -0300

    misc production

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,12 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
+const pool = new Pool(
+    process.env.NODE_ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+    }
+    : {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
